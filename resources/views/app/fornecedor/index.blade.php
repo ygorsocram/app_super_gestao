7041eefda571ca336739eecc44ee3b1e68161b82
Ygor <ygor.batista@grupocompeti.com.br>
2022-04-26 09:26:54 -0300

    Aula43

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,16 @@
+<h3>Fornecedor</h3>
+
+{{-- Fica o comentario descartado --}}
+{{'Texto1'}}
+<?='Texto2'?>
+
+@php
+//cometario de uma linha
+
+/*
+comantario
+muitas
+linhas
+*/
+echo 'teste3';
+@endphp
