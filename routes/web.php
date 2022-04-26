dcc3cecbd4844001a0b4e5a3128a165cc77e5f99
Ygor <ygor.batista@grupocompeti.com.br>
2022-04-26 08:57:30 -0300

    Aula40

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,7 @@ Route::prefix('/app')->group(function(){
     Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
 });
 
-Route::get('/rota1', function(){ echo 'Rota1'; })->name('site.rota1');
-Route::get('/rota2', function(){ return redirect()->route('site.rota1'); })->name('site.rota2');
-//Route::redirect('/rota2','/rota1');
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class,'teste'])->name('teste');
 
 Route::fallback(function() {
     echo 'A rota não existe. <a href="'.route('site.index').'"> clique Aqui </a>';
